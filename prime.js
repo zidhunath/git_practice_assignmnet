3ee068dd4689554d93ebcb3e82c63d3e1104f42d
zidhunath <zidhunath7@gmail.com>
2022-09-22 17:06:30 +0530

    new chnages factor to count, reverse to bag in palindrome

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let num=13;
-let factor=0;
- for(let i=0;i<=num;i--){	
+let count=0;
+ for(let i=0;i<=num;i++){	
    if(num%1===0){
-		factor++;
+		count++;
    }
  }
- if(factor==2){
+ if(count==2){
 	console.log("Prime");
  }else{
 	console.log("Not prime");
